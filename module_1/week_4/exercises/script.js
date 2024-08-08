cfcac6faacae749a6ab4689a056e56f7f0583d4e
matheusbrandalise <matheusjosebrandalise@gmail.com>
2024-08-07 22:16:40 -0300

    fazendo exercisios da segunda parte da aula 3 da semana 3 e iniciando exercícios da semana 4 #main

---
+++ module_1/week_4/exercises/script.js
@@ -0,0 +1,22 @@
+document.getElementById("calculator-button").addEventListener("click", function () {
+    // Chamar a função sum() quando o botão for clicado
+    sum();
+  });
+
+function sum() {
+  // Capturar os valores dentro da função para garantir que sejam lidos no momento do clique
+  const valueOne = parseFloat(document.getElementById("inputNumberOne").value);
+  const valueTwo = parseFloat(document.getElementById("inputNumberTwo").value);
+
+  // Verificar se ambos os números são válidos
+  if (!isNaN(valueOne) && !isNaN(valueTwo)) {
+    // Realizar a soma
+    const result = valueOne + valueTwo
+
+    // Exibir o resultado
+    document.getElementById('resultDisplay').textContent = "A soma é " + result;
+  } else {
+    // Mensagem de erro se algum número não for válido
+        document.getElementById('resultDisplay').textContent = "Por favor, digite dois números válidos.";
+  }
+};
